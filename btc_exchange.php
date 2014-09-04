dec64cb131e40cc650c2db1c02695badea8a9258
csmicfool <csmicfool@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 22:49:48 +0000

    4.0 update
    git-svn-id: https://plugins.svn.wordpress.org/btc-exchange-widget/trunk@980716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- btc_exchange.php
+++ btc_exchange.php
@@ -7,7 +7,7 @@ Plugin Name: BTC Exchange Widget
 Plugin URI: http://jacobbaron.net
 Description: Bitcoin exchange rates and conversion tools.
 Author: csmicfool
-Version: 1.2.4
+Version: 1.2.5
 Author URI: http://jacobbaron.net
 */
 
